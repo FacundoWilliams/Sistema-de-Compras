2f195d7172f3e8d8ef7ef4660db86c25cd792fec
FacundoWilliams <45250454+FacundoWilliams@users.noreply.github.com>
2020-12-18 01:53:40 -0300

    Errores descriptivos para 403 y 404

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,10 @@
+<x-app-layout>
+
+    <x-slot name="header">
+
+        <h2 class="font-bold text-xl text-blue-800 leading-tight">
+            Usted no posee los permisos necesarios para acceder a esta función.
+        </h2>
+
+    </x-slot>
+    </x-app-layout>
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,10 @@
+<x-app-layout>
+
+    <x-slot name="header">
+
+        <h2 class="font-bold text-xl text-blue-800 leading-tight">
+            Página no encontrada.
+        </h2>
+
+    </x-slot>
+    </x-app-layout>
